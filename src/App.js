1bed7c69acf8b16d717301886e141458c33c3a71
ZhangYuP <zyp5318@qq.com>
2019-07-02 15:15:52 +0800

    v4

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component{
         <div>
           <button id="add1" onClick={()=> this.props.add1()}>+1</button>
           <button id="add2" onClick={()=> this.props.add2()}>+2</button>
-          <button id="add1IfOdd" onClick={()=> this.props.add3()}>如果是奇数就+1</button>
+          <button id="add1IfOdd" onClick={()=> this.props.add3(this.props.n)}>如果是奇数就+1</button>
           <button id="add1After2Sec" onClick={()=> this.props.add4()}>两秒钟后+1</button>
         </div>
       </div>
@@ -26,7 +26,8 @@ function mapDispatchToProps(dispatch){
   return {
     add1: ()=> dispatch({type: 'add', payload: 1}),
     add2: ()=> dispatch({type: 'add', payload: 2}),
-    add3: ()=> {
+    add3: (n)=> {
+      if (n % 2 === 1) dispatch({type: 'add', payload: 1})
     },
     add4: ()=> {
       setTimeout(()=> {
